beab0369135fb0ff1e06a9a0bb7d3c9a2e60e688
portalSDK <istptadm@uwaterloo.ca>
2016-10-29 11:15:26 -0400

    Created new file: javascript.js

---
+++ javascript.js
@@ -0,0 +1,47 @@
+angular.module('portalApp')
+.controller('exampracticeCtrl', ['$scope', function ($scope) {
+	
+	// mock data
+	$scope.items = [
+		{
+			title:'Item 1',
+			tags: ['tag A', 'tag B', 'tag C'],
+			details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+		},
+		{
+			title:'Item 2',
+			tags: ['tag D', 'tag E', 'tag F'],
+			details: 'Mauris cursus, sapien et malesuada ultrices, purus sapien iaculis tellus, quis semper magna est at leo.'
+		},
+		{
+			title:'Item 3',
+			tags: ['tag A', 'tag H'],
+			details: 'Donec id quam eu odio feugiat sagittis. Duis a tempus neque. Praesent elementum quis ante quis commodo. Sed tincidunt aliquet dolor sit amet laoreet. '
+		},
+		{
+			title:'Item 4',
+			tags: ['tag I'],
+			details: 'Proin sem quam, rutrum id ante id, scelerisque tempor quam. Curabitur pharetra turpis at sem placerat, non vehicula ligula tincidunt.'
+		},
+		{
+			title:'Item 5',
+			tags: ['tag C', 'tag K', 'tag B'],
+			details: 'Mauris nec ultricies metus. Cras et dictum justo. Nam a ullamcorper dolor. Cras fringilla metus vel facilisis vehicula.'
+		},
+		{
+			title:'Item 6',
+			tags: ['tag A', 'tag B', 'tag C'],
+			details: 'Curabitur scelerisque lorem risus, in luctus orci hendrerit non. Praesent quis tellus dapibus dolor consectetur volutpat.'
+		}
+	];
+	
+	// Show main view in the first column as soon as controller loads
+	$scope.portalHelpers.showView('exampracticeMain.html', 1);
+	
+	// This function gets called when user clicks an item in the list
+	$scope.showDetails = function(item){
+		// Make the item that user clicked available to the template
+		$scope.detailsItem = item;		
+		$scope.portalHelpers.showView('exampracticeDetails.html', 2);
+	}
+}]);
